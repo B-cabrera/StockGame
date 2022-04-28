d8a650a6be8eeba68b6ce86d52dbf58242626659
B-cabrera <brendenc29@gmail.com>
2022-04-27 22:42:25 -0400

    Added comments and moved exit method

--- player.py
+++ player.py
@@ -6,8 +6,15 @@ class Player(object):
     def __init__(self):
         self.__balance = self.STARTING_BALANCE
         self.is_broke = False
-        self.is_in_trade = False
+        self.__in_trade = False
 
     
     def get_balance(self) -> int:
         return self.__balance
+
+
+    def is_in_trade(self) -> bool:
+        return self.__in_trade
+
+
+    
--- playingfield.py
+++ playingfield.py
@@ -5,29 +5,23 @@ from pygame import font
 
 
 class PlayingField(object):
-
+    
+    # Constants for window size
     WINDOW_HEIGHT = 600
     WINDOW_WIDTH = 1000
 
     def __init__(self):
         pygame.init()
-        self.window  = pygame.display
         him = Player()
 
+        # Setting up the window 
+        self.window  = pygame.display
         self.surface = self.window.set_mode((self.WINDOW_WIDTH, self.WINDOW_HEIGHT))
         self.window.set_caption("Brenden Cabrera Stock Game")
 
-        self.show_balance(him)
+        self.show_starting_balance(him)
 
-        self.exit_scenario()
-
-        
-
-        
-
-    
-    # Code for the exit scenario (Closing Window)
-    def exit_scenario(self):
+        # Running Loop
         running = True
 
         while running:
@@ -37,22 +31,24 @@ class PlayingField(object):
                     running = False
 
 
+
         pygame.quit()
 
-    
+        
 
-    def show_balance(self, player: Player) :
+
+    def show_starting_balance(self, player: Player) :
         pygame.font.init()
 
         # Setting basic color
         white = (255,255,255)
 
-        basicfont = font.SysFont('arial', 40)
+        basicfont = font.SysFont('cambria', 40)
         balance = player.get_balance()
 
         text = basicfont.render("Balance: " + str(balance), True, white)
 
-        self.surface.blit(text, (400,0))
+        self.surface.blit(text, (10,0))
         self.window.flip()
 
         
